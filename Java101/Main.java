e58b890b897d0411408589fafa232e22873e6512
kasey <xiakasey25@gmail.com>
2024-12-01 19:39:42 -0500

    finished java 101

---
+++ Java101/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] arr = {4, 3, 2, 8, 3};
+
+        System.out.println(PracticeProblems.countOccurrences(arr, 3));
+    }
+}
---
+++ Java101/PracticeProblems.java
@@ -0,0 +1,43 @@
+public class PracticeProblems {
+    //practice question 1
+    public static int countOccurrences(int[] arr, int n) {
+        int count = 0; 
+        for (int element : arr) {
+            if (element == n) { 
+                count++; 
+            }
+        }
+        return count; 
+    }
+    //practice question 2
+    public static int[] reverseArray (int[] arr){
+         int[] reversed = new int[] {arr.length};
+
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
+            }
+    return reversed;
+        }
+    //practice problem 3
+    public static int sumGrid (int [][] grid) {
+        int sum= 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                sum += grid[i][j];
+            }
+        }
+        return sum;
+    }
+    //practice problem 4
+
+        // Function to calculate the nth Fibonacci number
+        public static int fib(int n) {
+            if (n == 0) {
+                return 0; 
+            } else if (n == 1) {
+                return 1; 
+            }
+    
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
